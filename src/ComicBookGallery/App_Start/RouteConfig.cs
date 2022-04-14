932692679445d48fbc55a1b86a03e6b3fc37d0f9
Brittany Jones <brittany.jones@nuby.com>
2022-04-14 15:36:52 -0500

    fixed typo

--- src/ComicBookGallery/App_Start/RouteConfig.cs
+++ src/ComicBookGallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComicBookGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ComicBook", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBooks", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
